7833ea91bd82c8d1a1c7750d2e9f2e92374ac71e
Maksim Evstafev <emaxiss@gmail.com>
2020-11-04 23:59:55 -0500

    templatetests

--- src/test/java/us/pasv/TemplateTest.java
+++ src/test/java/us/pasv/TemplateTest.java
@@ -2,33 +2,47 @@ package us.pasv;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TemplateTest
 {
+	@BeforeAll
+	static void oneTimeSetUp()
+	{
+		System.out.println("BeforeAll - oneTimeSetUp");
+	}
+
 	@BeforeEach
 	void setUp()
 	{
 		System.out.println("@BeforeEach - setUp");
 	}
-	
+
 	@Test
 	void test1()
 	{
 		System.out.println("test1");
 	}
-	
+
 	@Test
 	void test2()
 	{
 		System.out.println("test2");
 	}
-	
+
 	@AfterEach
 	void tearDown()
 	{
 		System.out.println("@AfterEach - tearDown");
+	}
+	
+	@AfterAll
+	static void oneTimeTearDown()
+	{
+		System.out.println("@AfterAll - oneTimeTearDown");
 	}
 }
