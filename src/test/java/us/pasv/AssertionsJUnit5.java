586a43732eea31ad996ee6506811f58dc79d8073
Maksim Evstafev <emaxiss@gmail.com>
2020-11-10 00:31:13 -0500

    assertions

---
+++ src/test/java/us/pasv/AssertionsJUnit5.java
@@ -0,0 +1,27 @@
+package us.pasv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AssertionsJUnit5
+{
+	@DisplayName("Standart assertions")
+	@Test
+	void test()
+	{
+		assertEquals(2, 1 + 1, "sum 1 + 1 must be 2");
+		assertTrue(3 > 1);
+		assertFalse(1 > 11, "if 1 greater than 11");
+	}
+	
+	@Test
+	void perfomanceTest()
+	{
+		assertTimeout(Duration.ofMillis(10),
+				() -> {Thread.sleep(8);});
+	}
+}
