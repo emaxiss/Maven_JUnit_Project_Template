b9c6b18995e145d1461ba0465e1c9e2456e0fc14
Maksim Evstafev <emaxiss@gmail.com>
2020-11-12 00:38:27 -0500

    data driven tests

---
+++ src/test/java/us/pasv/ParametirizedTests.java
@@ -0,0 +1,14 @@
+package us.pasv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParametirizedTests
+{
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+}
